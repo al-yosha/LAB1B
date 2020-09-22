f41656a370122ae2b83747bc5a6d66ff7ec84dc0
al-yosha <ivalmi2019@gmail.com>
2020-09-22 12:53:36 +0300

    Большой блок тестов был разбит на несколько маленьких

--- test.cpp
+++ test.cpp
@@ -2,7 +2,7 @@
 #include "gtest/gtest.h"
 #include "..\LAB1B\Nephroid.h"
 #include "..\LAB1B\Nephroid.cpp"
-#include <sstream>
+
 
 TEST(NephroidConstructor, Constructor1) {
 	lab1b::Nephroid c1;
@@ -11,8 +11,7 @@ TEST(NephroidConstructor, Constructor1) {
 	ASSERT_EQ(1, c1.getR());
 }
 
-TEST(NephroidConstructor, InitConstructors)
-{
+TEST(NephroidConstructor, InitConstructors) {
 	lab1b::Nephroid a2(3);
 	lab1b::Point p(2, 3);
 	ASSERT_EQ(3, a2.getR());
@@ -27,15 +26,13 @@ TEST(NephroidConstructor, InitConstructors)
 	ASSERT_EQ(-5, a4.getP().y);
 	ASSERT_EQ(3, a4.getR());
 }
-TEST(NephroidConstructor, TestException)
-{
+TEST(NephroidConstructor, TestException) {
 	lab1b::Point p;
 	ASSERT_ANY_THROW(lab1b::Nephroid(2, 5, -5));
 	ASSERT_ANY_THROW(lab1b::Nephroid(p, -1));
 }
 
-TEST(NephroidMethods, Setters)
-{
+TEST(NephroidMethods, Setters) {
 	lab1b::Nephroid a;
 	lab1b::Point p1(3, 2);
 	a.setP(p1);
@@ -50,29 +47,54 @@ TEST(NephroidMethods, Setters)
 	ASSERT_ANY_THROW(a.setR(-1));
 }
 
-TEST(NephroidMethods, Parameters)
-{
+TEST(NephroidMethods, Equation) {
 	lab1b::Nephroid a1;
 	const double PI = 3.14159, err = 0.0001;
-	ASSERT_NEAR(24, a1.length(), err);
-	ASSERT_EQ(0, a1.curveradius());
-	ASSERT_EQ(2, a1.coordinates().x);
-	ASSERT_EQ(0, a1.coordinates().y);
-	ASSERT_NEAR(0, a1.coordinates(PI/2).x, err);
-	ASSERT_NEAR(4, a1.coordinates(PI/2).y, err);
 	std::stringstream s;
 	s << "(x^2 + y^2 - 4.00)^3 = 108.00*x^2" << std::endl;
 	ASSERT_STREQ(s.str().c_str(), a1.equation().c_str());
 	lab1b::Nephroid a2(1, 3, 2);
-	ASSERT_NEAR(48, a2.length(), err);
-	ASSERT_EQ(0, a2.curveradius());
+	s.str("");
+	s << "((x - 1.00)^2 + (y - 3.00)^2 - 16.00)^3 = 1728.00*(x - 1.00)^2" << std::endl;
+	ASSERT_STREQ(s.str().c_str(), a2.equation().c_str());
+}
+
+TEST(NephroidMethods, Coordinates) {
+	lab1b::Nephroid a1;
+	const double PI = 3.14159, err = 0.0001;
+	ASSERT_EQ(2, a1.coordinates().x);
+	ASSERT_EQ(0, a1.coordinates().y);
+	ASSERT_NEAR(0, a1.coordinates(PI / 2).x, err);
+	ASSERT_NEAR(4, a1.coordinates(PI / 2).y, err);
+	lab1b::Nephroid a2(1, 3, 2);
 	ASSERT_EQ(5, a2.coordinates().x);
 	ASSERT_EQ(3, a2.coordinates().y);
 	ASSERT_NEAR(1, a2.coordinates(PI / 2).x, err);
 	ASSERT_NEAR(11, a2.coordinates(PI / 2).y, err);
-	s.str("");
-	s << "((x - 1.00)^2 + (y - 3.00)^2 - 16.00)^3 = 1728.00*(x - 1.00)^2" << std::endl;
-	ASSERT_STREQ(s.str().c_str(), a2.equation().c_str());
+}
+
+TEST(NephroidMethods, Length) {
+	lab1b::Nephroid a1;
+	const double PI = 3.14159, err = 0.0001;
+	ASSERT_NEAR(24, a1.length(), err);
+	lab1b::Nephroid a2(1, 3, 2);
+	ASSERT_NEAR(48, a2.length(), err);
+}
+
+TEST(NephroidMethods, Curveradius) {
+	lab1b::Nephroid a1;
+	const double PI = 3.14159, err = 0.0001;
+	ASSERT_EQ(0, a1.curveradius());
+	lab1b::Nephroid a2(1, 3, 2);
+	ASSERT_EQ(0, a2.curveradius());
+}
+
+TEST(NephroidMethods, Area) {
+	lab1b::Nephroid a1;
+	const double PI = 3.1415, err = 0.0001;
+	ASSERT_EQ(12 * PI, a1.area());
+	lab1b::Nephroid a2(1, 3, 2);
+	ASSERT_EQ(48 * PI, a2.area());
 }
 
 int main(int argc, char* argv[])
